a8910e83f46cabc27256e6e1b5b13563207e3302
Baron <161209670+7ucg@users.noreply.github.com>
2025-04-27 18:13:07 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,10 @@
-const webpack = require('webpack');
+
 
 module.exports = {
   mode: 'development', // oder 'production'
   entry: './server.js', // oder dein richtiger Pfad
-  resolve: {
-    fallback: {
-    
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "buffer": require.resolve("buffer/"),
-      "process": require.resolve("process/browser")
-    }
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser',
-    }),
-  ],
-  // ggf. noch output und weitere Einstellungen
+
+
+ 
+ 
 };
